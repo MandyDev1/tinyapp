2b62353adf12fe6e1b2ec52a8f37008e22a33ef3
Mandy Liu <manling512@gmail.com>
2023-07-21 17:07:41 -0600

    Redirect shortURL to longURL

--- express_server.js
+++ express_server.js
@@ -5,6 +5,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 6;
+  let randomString = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters[randomIndex];
+  }
+
+  return randomString;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -41,19 +54,23 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+
+  // Save the id-longURL pair to the urlDatabase;
+  urlDatabase[shortURL] = longURL;
+
+  // Redirect the user to the new short URL they created
+  res.redirect(`/urls/${shortURL}`);
 });
 
-function generateRandomString() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const length = 6;
-  let randomString = '';
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
 
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    randomString += characters[randomIndex];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("Short URL not found");
   }
-
-  return randomString;
-};
+});
