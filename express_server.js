7ebfdab592b428b4891cc031975832a4a80e5d28
Mandy Liu <manling512@gmail.com>
2023-07-21 18:18:12 -0600

    Able to delete urls

--- express_server.js
+++ express_server.js
@@ -64,6 +64,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
